c8eaf0f2390f9f4f878ec68da019a7cc0f8e18ad
Phillipa Bennett-Eghan <62352130+abena07@users.noreply.github.com>
2021-02-19 21:57:57 +0000

    first commit

---
+++ forrrloopy.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# question generating a 3*3 matrix with  9 random prime numbers using a for loop in python
+
+
+# In[13]:
+
+
+#a 3 by 3 matrix is a matrix that has 3 rows and 3 columns 
+
+[1,1,2],
+[2,3,4],
+[5,6,7],
+
+
+#a prime number is a number that is divisible only by itself and 1 eg:1,2,3,5,7,11
+[1,5,7,1],
+[3,11,13,17],
+[2,5,7,3]
+
+#hint you need to learn numpy to do this
+#if you can do it w/o numpy lmk
+
+
+# In[12]:
+
+
+list1=[1,3,5,7]
+list2=[1,3,5,7]
+prime=[i/j for i,j in zip(list1,list2)]
+print (prime)
+
+
+# In[ ]:
+
+
+
+
