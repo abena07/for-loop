65c54e28fb549f3e192936b98426e43eebd7627b
Phillipa Bennett-Eghan <62352130+abena07@users.noreply.github.com>
2021-02-19 22:01:16 +0000

    Add files via upload

---
+++ learning python.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+n=1
+print(n)
+
+
+# In[3]:
+
+
+t,f =True, False
+print(t,f)
+
+
+# In[5]:
+
+
+get_ipython().system('python --version')
+
+
+# In[6]:
+
+
+#String Operations
+len("Ama B-hairs")
+
+
+# In[7]:
+
+
+#to get the length of a variable it has to be a string
+len("13.04")
+
+
+# In[8]:
+
+
+name="jason"
+
+print(name)
+name = name + " dela"
+print(name)
+
+
+# In[18]:
+
+
+x= "9"+"9"
+print(x)
+#in this case the numbers are being joined together not added
+y=9+9
+print(y)
+#here they are being added
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
