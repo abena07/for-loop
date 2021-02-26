fb89f59d722fd06055340a0481889cb3e151e784
Phillipa Bennett-Eghan <62352130+abena07@users.noreply.github.com>
2021-02-26 15:46:59 +0000

    Add files via upload

---
+++ introduction-to-ML.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#machine learning workflow 
+#first step - extract features
+#split data set into two data sets: tests set and train sets
+#train the model
+# evalute the model
+#ps if the model is not god enough you tune the model and go back to training the model
+#2 types of machine learning : supervised and unsupervised 
+#deep learning is based on neural networks
+#computer vision is used by tesla,bmw
+#nlp is uses text it is used for apps like google translator,chat boxes
+#Limitations of machine learning- data quality
+
+
+# In[ ]:
+
+
+
+
